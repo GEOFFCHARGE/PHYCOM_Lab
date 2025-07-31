76d623f791630b6fcf9319b9fb43d337ec6da808
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-07-31 22:56:06 +0700

    Update 05-10.c

--- Week_05/Lab/05-10.c
+++ Week_05/Lab/05-10.c
@@ -4,29 +4,29 @@
 
 int main()
 {
-	int scam = 0, stat;
-	float owns = 0.0, acct = 0.0, cash;
-	scanf("%f\n%f", &acct, &owns);
-	while (scam < 3 && stat != 'E')
-	{
-		scanf(" %c %f", &stat, &cash);
-		if (stat == 'D' && owns >= cash)
-		{
-			owns -= cash;
-			acct += cash;
-			scam = 0;
-		}
-		else if (stat == 'W' && acct >= cash)
-		{
-			acct -= cash;
-			owns += cash;
-			scam = 0;
-		}
-		else
-		{
-			scam++;
-		}
-	}
-	printf("%.2f\n%.2f", acct, owns);
-	return 0;
+    int scam = 0, stat;
+    float owns = 0.0, acct = 0.0, cash;
+    scanf("%f\n%f", &acct, &owns);
+    while (scam < 3 && stat != 'E')
+    {
+        scanf(" %c %f", &stat, &cash);
+        if (stat == 'D' && owns >= cash)
+        {
+            owns -= cash;
+            acct += cash;
+            scam = 0;
+        }
+        else if (stat == 'W' && acct >= cash)
+        {
+            acct -= cash;
+            owns += cash;
+            scam = 0;
+        }
+        else
+        {
+            scam++;
+        }
+    }
+    printf("%.2f\n%.2f", acct, owns);
+    return 0;
 }
