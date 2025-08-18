9baa5ee32aef97df267611b9b47de8ca26241455
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-08-18 20:41:49 +0700

    Update 07-11.c

--- Week_07/Lab/07-11.c
+++ Week_07/Lab/07-11.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    int totalSecond;
-    long int totalMinute, queueSecond = 0;
+int main()
+{
+    int totalSecond, queueLength, percentages;
+    long int totalMinute, processings, queueSecond = 0, currentings = 0;
     scanf("%ld.%d", &totalMinute, &totalSecond);
     long int totalTiming = totalMinute * 60 + totalSecond;
-    int queueLength;
     scanf("%d", &queueLength);
     int musicNumber[queueLength], musicLength[queueLength];
     char musicNaming[queueLength][52];
-    for (int i = 0; i < queueLength; i++) {
+    for (int i = 0; i < queueLength; i++)
+    {
         int queue, minute, second;
         char names[52];
         scanf(" Queue#%d <|> %[^<] <|> %d.%d", &queue, &names, &minute, &second);
@@ -22,24 +23,29 @@ int main() {
         queueSecond += musicLength[queue - 1];
     }
     totalTiming %= queueSecond;
-    if (totalTiming == 0) {
+    if (totalTiming == 0)
+    {
         totalTiming = queueSecond;
     }
-    long int currentings = 0;
-    for (int i = 0; i < queueLength; i++) {
+    for (int i = 0; i < queueLength; i++)
+    {
         currentings += musicLength[i];
-        if (currentings >= totalTiming) {
+        if (currentings >= totalTiming)
+        {
             printf("Song Order: %d\n", musicNumber[i]);
             printf("Song Name: %s\n", musicNaming[i]);
-            long int processings = totalTiming - (currentings - musicLength[i]);
-            int percentages = (int) ((double) processings / musicLength[i] * 100);
-            if (percentages == 0) {
+            processings = totalTiming - (currentings - musicLength[i]);
+            percentages = (int) ((double) processings / musicLength[i] * 100);
+            if (percentages == 0)
+            {
                 percentages = 1;
             }
-            if (percentages == 100) {
+            if (percentages == 100)
+            {
                 printf("Song Process: Complete\n");
             }
-            else {
+            else
+            {
                 printf("Song Process: %d%%\n", percentages);
             }
             break;
