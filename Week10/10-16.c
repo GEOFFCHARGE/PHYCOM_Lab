f1cc34d65339e0628ddcabdce50b4616b7b182f0
GEOFFCHARGE <office.wanlachai@gmail.com>
2025-09-14 18:17:27 +0700

    Add Week10 Linked List

---
+++ Week10/10-16.c
@@ -0,0 +1,150 @@
+// Lab 10-16 Linked List - Delete
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct DataNode
+{
+    char* data;
+    struct DataNode* next;
+} DataNode;
+
+typedef struct SinglyLinkedList
+{
+    unsigned int count;
+    DataNode* head;
+} SinglyLinkedList;
+
+DataNode* createDataNode(char* data)
+{
+    DataNode* newNode = (DataNode*) malloc(sizeof(DataNode));
+    newNode->data = (char*) malloc(strlen(data) + 1);
+    strcpy(newNode->data, data);
+    newNode->next = NULL;
+    return newNode;
+}
+
+SinglyLinkedList* createSinglyLinkedList()
+{
+    SinglyLinkedList* list = (SinglyLinkedList*) malloc(sizeof(SinglyLinkedList));
+    list->head = NULL;
+    list->count = 0;
+    return list;
+}
+
+void traverse(SinglyLinkedList* list)
+{
+    if (list->count == 0)
+    {
+        printf("This is an empty list.\n");
+        return;
+    }
+    struct DataNode* pointer = list->head;
+    while (pointer->next != NULL)
+    {
+        printf("%s -> ", pointer->data);
+        pointer = pointer->next;
+    }
+    printf("%s\n", pointer->data);
+}
+
+void insert_front(SinglyLinkedList* list, char* data)
+{
+    DataNode* pNew = createDataNode(data);
+    if (list->count == 0)
+    {
+        list->head = pNew;
+    }
+    else
+    {
+        pNew->next = list->head;
+        list->head = pNew;
+    }
+    list->count++;
+}
+
+void insert_last(SinglyLinkedList* list, char* data)
+{
+    DataNode* pNew = createDataNode(data);
+    if (list->count == 0) 
+    {
+        list->head = pNew;
+    }
+    else
+    {
+        DataNode* pointer = list->head;
+        while (pointer->next != NULL)
+        {
+            pointer = pointer->next;
+        }
+        pointer->next = pNew;
+    }
+    list->count++;
+}
+
+void delete(SinglyLinkedList* list, char* data)
+{
+    DataNode* current = list->head;
+    DataNode* previous = NULL;
+    while (current != NULL)
+    {
+        if (!strcmp(current->data, data))
+        {
+            if (previous == NULL)
+            {
+                list->head = current->next;
+            }
+            else
+            {
+                previous->next = current->next;
+            }
+            free(current->data);
+            free(current);
+            list->count--;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+    printf("Cannot delete, %s does not exist.\n", data);
+}
+
+int main() {
+    SinglyLinkedList* mylist = createSinglyLinkedList();
+    int n;
+    char condition;
+    char data[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %c: %[^\n]s", &condition, data);
+        if (condition == 'F')
+        {
+            insert_front(mylist, data);
+        }
+        else if (condition == 'L')
+        {
+            insert_last(mylist, data);
+        }
+        else if (condition == 'D')
+        {
+            delete(mylist, data);
+        }
+        else
+        {
+            printf("Invalid Condition!\n");
+        }
+    }
+    traverse(mylist);
+    DataNode* current = mylist->head;
+    while (current != NULL)
+    {
+        free(current->data);
+        DataNode* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    free(mylist);
+    return 0;
+}
