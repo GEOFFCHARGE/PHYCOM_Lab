42ace0af555343cf7dce9c95b6c7f43be9a9fb7f
Wanlachai Klammuang <147297706+GEOFFCHARGE@users.noreply.github.com>
2025-10-01 15:55:17 +0700

    Add files via upload

---
+++ Week13/Thermistor_Sensor.ino
@@ -0,0 +1,23 @@
+const int therPin = A0;
+
+int count = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    float Read = (5.00 / 1023.00) * analogRead(therPin);
+    float VR = 5 - Read;
+    float RT = Read / (VR / 330);
+    float ln = log(RT / 10000);
+    float Temp = (1 / ((ln / 3977) + (1 / (25 + 273.15)))) - 273.15;
+    Serial.print("Temperature ");
+    Serial.print(count++);
+    Serial.print(": ");
+    Serial.print(Temp);
+    Serial.println(" C");
+    delay(1000); 
+}
